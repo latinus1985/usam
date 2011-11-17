3312cd82a8c5f2c1539539dcda60b3851185b149
Daniel <znowm4n@gmail.com>
2011-11-17 19:49:18 +0100

    created USAM app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usam::Application.config.secret_token = 'ef497e97bbd9d52d7cd4caf0f451e52d012ff33d4e7d80aa00124a8ea2642601f055573956667e5a43663efd3afd54b9d04733c5d4b5e370cdcb2312be80a536'
